5acf3daeac938ccc44d690ff4127ed64778cc5e8
MiguelAngel0425 <miguel14caiza@gmail.com>
2025-01-27 12:01:25 -0500

    Refact

--- SISTEMA DE PARQUEO USANDO ARBOL B/DataEncryption.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <string>
-
-class CesarCipher {
-public:
-    static std::string encrypt(const std::string& text, int shift = 3);
-    static std::string decrypt(const std::string& text, int shift = 3);
-};
